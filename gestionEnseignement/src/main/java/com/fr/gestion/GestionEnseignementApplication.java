dcaee245636ca8fcae73877a5d4454c081fb041e
Saab <sabrinekhemici@gmail.com>
2021-11-09 22:28:19 +0100

    Initiaisation de l'architecture

---
+++ gestionEnseignement/src/main/java/com/fr/gestion/GestionEnseignementApplication.java
@@ -0,0 +1,13 @@
+package com.fr.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEnseignementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEnseignementApplication.class, args);
+	}
+
+}
---
+++ gestionEnseignement/src/test/java/com/fr/gestion/GestionEnseignementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEnseignementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
