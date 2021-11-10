16db44d85c9a3cd7522a97d8f048e904d0f52db7
BERRAI <kaabiaroua@gmail.com>
2021-11-10 16:47:25 +0100

    modification interface service

--- gestionEnseignement/src/main/java/com/fr/gestion/service/IEnseignantService.java
+++ gestionEnseignement/src/main/java/com/fr/gestion/service/IEnseignantService.java
@@ -1,9 +1,5 @@
 package com.fr.gestion.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fr.gestion.persistence.entities.EnseignantDo;
-
-public interface IEnseignantService extends JpaRepository<EnseignantDo, Integer> {
+public interface IEnseignantService {
 
 }
