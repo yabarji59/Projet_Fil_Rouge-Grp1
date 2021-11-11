846a984cb67806baad9cb48b2b03aa7280068577
Saab <sabrinekhemici@gmail.com>
2021-11-12 00:23:12 +0100

    findCoursById, findAll, delete ok

--- gestionEnseignement/src/main/java/com/fr/gestion/service/impl/CoursServiceImp.java
+++ gestionEnseignement/src/main/java/com/fr/gestion/service/impl/CoursServiceImp.java
@@ -1,7 +1,73 @@
 package com.fr.gestion.service.impl;
 
-import com.fr.gestion.service.ICoursService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fr.gestion.persistence.entities.CoursDo;
+import com.fr.gestion.persistence.repository.ICoursDao;
+import com.fr.gestion.service.ICoursService;
+import com.fr.gestion.web.model.CoursDto;
+
+@Service
 public class CoursServiceImp implements ICoursService {
+
+	@Autowired
+	private ICoursDao coursDao;
+
+	@Override
+	public List<CoursDto> findAllCours() {
+		List<CoursDo> coursList = this.coursDao.findAll();
+		return mapToListCoursDto(coursList);
+	}
+
+	private CoursDto mapToCoursDto(CoursDo coursDo) {
+		CoursDto coursDto = new CoursDto();
+		if (coursDo == null) {
+			return null;
+		}
+
+		coursDto.setIdCours(coursDo.getIdCours());
+		coursDto.setLibelle(coursDo.getLibelle());
+		return coursDto;
+	}
+
+	private List<CoursDto> mapToListCoursDto(final List<CoursDo> listCoursDo) {
+		final List<CoursDto> listCoursDto = new ArrayList<>();
+		for (CoursDo coursDo : listCoursDo) {
+			listCoursDto.add(mapToCoursDto(coursDo));
+		}
+
+		return listCoursDto;
+	}
+
+	@Override
+	public void deleteCours(int idCours) {
+		this.coursDao.deleteById(idCours);
+	}
+
+	@Override
+	public CoursDto findByIdCours(int idCours) {
+		Optional<CoursDo> coursDoOptional=this.coursDao.findById(idCours);
+		if(coursDoOptional.isEmpty()) {
+			return null;
+		}
+		return mapToCoursDto(coursDoOptional.get());
+	}
+
+	@Override
+	public void createCours(CoursDto cours) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCours(CoursDto cours) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
