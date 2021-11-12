89adba5046f3cf9ceaeafc48f448f94c3b90f8c3
Saab <sabrinekhemici@gmail.com>
2021-11-12 22:10:48 +0100

    create,update cours

--- gestionEnseignement/src/main/java/com/fr/gestion/web/controller/CoursController.java
+++ gestionEnseignement/src/main/java/com/fr/gestion/web/controller/CoursController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,5 +49,17 @@ public class CoursController {
 	@GetMapping({ "/cours/{id}" })
 	public CoursDto findById(@PathVariable int id) {
 		return this.coursService.findByIdCours(id);
+	}
+
+	@PostMapping({ "/cours" })
+	public void save(@RequestBody CoursDto cours) {
+		this.coursService.createCours(cours);
+
+	}
+
+	@PutMapping({ "/cours" })
+	public void update(@RequestBody CoursDto cours) {
+		this.coursService.updateCours(cours);
+
 	}
 }
