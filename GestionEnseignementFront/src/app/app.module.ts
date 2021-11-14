514872668ddfa9f17790b46ba562f3cc204c1b66
Charline8380 <charline.abuwe@gmail.com>
2021-11-14 18:42:15 +0100

    structure projet front, and conf route

--- GestionEnseignementFront/src/app/app.module.ts
+++ GestionEnseignementFront/src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageAccueilComponent } from './page-accueil/page-accueil.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { AuthService } from './services/auth.service';
+import { UtilisateurComponent } from './pages/utilisateur/utilisateur.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+const appRoutes: Routes = [
+{ path: 'auth', component: AuthComponent },
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageAccueilComponent
+    PageAccueilComponent,
+    AuthComponent,
+    UtilisateurComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
