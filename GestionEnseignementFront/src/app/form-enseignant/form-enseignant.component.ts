326e14f3d1b27056dcd356fb4d94ccb1f12ef9b4
Miousss <90383325+Miousss@users.noreply.github.com>
2021-11-18 09:32:12 +0100

    Gestion enseignant

---
+++ GestionEnseignementFront/src/app/form-enseignant/form-enseignant.component.ts
@@ -0,0 +1,76 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { EnseignantService } from 'src/app/services/enseignant.service';
+import { Enseignant } from 'src/app/shared/model/enseignant.model';
+
+@Component({
+  selector: 'app-form-enseignant',
+  templateUrl: './form-enseignant.component.html',
+  styleUrls: ['./form-enseignant.component.css']
+})
+export class FormEnseignantComponent implements OnInit {
+
+  addEnseignantForm: FormGroup;
+  titre= '';
+  editForm= false;
+  enseignantGet!: string;
+
+  constructor(
+    private enseignantService: EnseignantService,
+    private matDialog: MatDialog,
+    public formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public dialogData: any) {
+      this.addEnseignantForm = this.formBuilder.group({
+        nom: ['', Validators.maxLength(1000)],
+        prenom: ['', Validators.maxLength(1000)],
+        age: ['', Validators.maxLength(1000)],
+        telephone: ['', Validators.maxLength(1000)],
+        email: ['', Validators.maxLength(1000)],
+        adresse: ['', Validators.maxLength(1000)],
+      });
+    if(this.dialogData.modalType == 'edit'){
+      this.titre ='Modifier un enseignant';
+      this.editForm= true;
+      this.enseignantGet= this.dialogData.enseignant;
+      this.addEnseignantForm.setValue({
+        nom: this.dialogData.enseignant.nom,
+        prenom: this.dialogData.enseignant.prenom,
+        age: this.dialogData.enseignant.age,
+        telephone: this.dialogData.enseignant.telephone,
+        email: this.dialogData.enseignant.email,
+        adresse: this.dialogData.enseignant.adresse
+      });
+    } else {
+      this.titre ='Ajouter un enseignant';
+    }
+    
+    }
+
+  ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.matDialog.closeAll();
+  }
+
+  addEleve(){
+    let enseignant= new Enseignant();
+    enseignant.nom = this.addEnseignantForm.get('nom')?.value;
+    enseignant.prenom = this.addEnseignantForm.get('prenom')?.value;
+    enseignant.age = this.addEnseignantForm.get('age')?.value;
+    enseignant.telephone = this.addEnseignantForm.get('telephone')?.value;
+    enseignant.email= this.addEnseignantForm.get('email')?.value;
+    enseignant.adresse = this.addEnseignantForm.get('adresse')?.value;
+    this.enseignantService.createEnseignant(enseignant).subscribe(
+        (      result: any) => {this.matDialog.closeAll();}
+    ,(errors: any) =>{});
+  }
+
+  updateEnseignant(){
+    this.enseignantGet.nom = this.addEnseignantForm.get('nom')?.value;
+    this.enseignantService.updateEnseignant(this.enseignantGet).subscribe(
+        (      result: any) => {this.matDialog.closeAll();}
+    ,(errors: any) =>{});
+  }
+}
